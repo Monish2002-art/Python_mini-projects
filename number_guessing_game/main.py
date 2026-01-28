8af0f68a69dc03aad8189ddedd2ed3142af364eb
Monish Kannan <monishkannan179@gmail.com>
2026-01-28 11:10:20 +0530

    Fix guessing range and improve win message

--- number_guessing_game/main.py
+++ number_guessing_game/main.py
@@ -6,27 +6,27 @@ chance = 0
 print("Welcome to number guessing game!!!")
 print("You have 6 chances")
 print("------------------------------")
-choice = int(input("Guess a number b/w (1,100):"))
+choice = int(input("Guess a number b/w (1,10):"))
 
 print("------------------------------")
 
 while chance < 6:
 
-    if choice == number:
-        print("Great, You've Won !!!!, congrats")
-
-    elif choice < number:
+    if choice < number:
         print("Your guess was low")
-        choice = int(input("Guess a number b/w (1,100):"))
+        choice = int(input("Guess a number b/w (1,10):"))
 
     elif choice > number:
         print("Your guess was higer")
-        choice = int(input("Guess a number b/w (1,100):"))
+        choice = int(input("Guess a number b/w (1,10):"))
 
-    else:
-        print("Guess b/w(1-100)")
+    elif choice == number:
+        print(f"Great, You've Won !!!!, congrats, the answer is {number}")
+
+        break
 
     chance +=1
+
 
     if chance == 6:
         print(f"your chance has been completed, the answer is {number}")
